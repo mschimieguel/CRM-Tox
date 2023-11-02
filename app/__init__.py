782c045408f16991b2324b04f39eac99bebc8f25
Matheus Schimieguel Silva <mschimieguel@gmail.com>
2023-11-02 11:21:16 -0300

    Criando documentacao Swagger

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template
-
+from flask_restx import Api,Namespace, Resource, fields
+api = Api()
 app = Flask(__name__)
+api.init_app(app)
  
-@app.route("/")
-def index():
-    return render_template("index.html")
+@api.route('/index', methods=['GET'])
+class IndexResource(Resource):
+    def get(self):
+        return render_template("index.html")
