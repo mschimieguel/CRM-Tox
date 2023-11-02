814cd6e3f7cc249db657090d9001cb0ef5e8ee33
Matheus Schimieguel Silva <mschimieguel@gmail.com>
2023-11-02 17:03:29 -0300

    adicionando mais rotas

--- app/__init__.py
+++ app/__init__.py
@@ -2,14 +2,14 @@ from flask import Flask, render_template
 from flask_restx import Api,Namespace, Resource, fields
 from flask_marshmallow import Marshmallow
 import os
-from flask import Flask, render_template, request, url_for, redirect,jsonify, make_response
+from datetime import datetime
+from flask import Flask, render_template, request, url_for, redirect,jsonify, make_response, abort
 from flask_sqlalchemy import SQLAlchemy
 
 from sqlalchemy.sql import func
 ma = Marshmallow()
 basedir = os.getcwd()
-print("basedir =============================")
-print(basedir)
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] =\
         'sqlite:///' + os.path.join(basedir, 'banco.db')
@@ -32,7 +32,18 @@ class Grupos(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(100), nullable=False)
     fonte = db.Column(db.String(100), nullable=False)
-    
+    dataExclusao = db.Column(db.DateTime, nullable=True)
+    def __init__(
+                self,
+                nome,
+                fonte,
+                dataExclusao=None
+            ):
+        self.id = id
+        self.nome = nome
+        self.fonte = fonte
+        self.dataExclusao = dataExclusao
+               
     def __repr__(self):
         return f'<Grupo {self.firstname}>'
 
@@ -43,15 +54,54 @@ class GrupoSchema(ma.SQLAlchemyAutoSchema):
 
     nome = fields.String(required=True, description="Nome Grupo")
     fonte = fields.String(required=False, description="fonte de dados")
-    
+    dataExlusao = fields.String(required=False, description="data de exclusao")
 
-@api.route('/grupo', methods=['GET'])
+@api.route('/grupo', methods=['GET','PUT'])
 class GrupoResource(Resource):
     def get(self):
         return listarTodosGrupos()
+    def put(self):
+         return cadastrarGrupo(id)
     
-   
+@api.route('/grupo/<id>', methods=['PATCH','DELETE'])
+class GrupoResource(Resource):
+    def patch(self,id):
+        return alterarGrupo(id)
+    def delete(self,id):
+        return deletarGrupo(id)
     
+def deletarGrupo(id):
+    cliente_obj = Grupos.query.filter_by(id=id).first()
+    if cliente_obj is None:
+        codigo, mensagem = bad_request("Cliente Nao Encontrado")
+        abort(codigo, mensagem)
+    dataExclusao =  datetime.now()
+    setattr(cliente_obj, 'dataExclusao', dataExclusao)
+    db.session.commit()
+    return True
+
+def cadastrarGrupo(id):
+    return True
+
+def alterarGrupo(id):
+    return True
+
 def listarTodosGrupos():
     result = Grupos.query.all()
     return GrupoSchema(many=True).jsonify(result)
+
+def success(data:dict = None):
+    response = {
+        'success': True,
+        'message': 'Operation successful',
+        'data': data
+     }
+    return jsonify(response), 200
+
+
+def bad_request(mensagem: str):
+    response = {
+        'success': False,
+        'message': "Bad request: " + mensagem
+    }
+    return 400,response
