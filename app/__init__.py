388828f458b459e96233e9920195f9f5606373d9
Matheus Schimieguel Silva <mschimieguel@gmail.com>
2023-11-03 23:07:45 -0300

    Refactoring: Movendo funcoes de validacao para arquivo separado

--- app/__init__.py
+++ app/__init__.py
@@ -14,7 +14,7 @@ from datetime import datetime
 import re
 from datetime import datetime
 import json
-
+import packages.validadores as validadores
 
 def converter_AAAA_MM_DD(data):
     if data is None:
@@ -217,7 +217,7 @@ class ClientePJResource(Resource):
         return listarPessoaJuridica()
 
 @nsCliente.route('/listarPessoaFisica', methods=['GET'])
-class ClientePFResource(Resource):
+class ClientePFResnomeAtributoource(Resource):
     def get(self):
         return listarPessoaFisica()
     
@@ -254,11 +254,11 @@ def cadastrarCliente():
     resultado,mensagem = pipelineValidacoes.Executar(
         [
             ('nome' in response_data,"Para cadastrar um cliente é preciso de um nome"),
-            (validarCPF_CNPJ(CPF_CNPJ), "CPF ou CNPJ invalido."),
-            (validarEmail(email), "Email Invalido"),
-            (validarTelefoneFixo(telefone), "Telefone Fixo Invalido"),
-            (validarData(dataNascimento), "Data de nascimento Invalida"),
-            (validarCelular(celular), "Celular Invalido"),
+            (validadores.validarCPF_CNPJ(CPF_CNPJ), "CPF ou CNPJ invalido."),
+            (validadores.validarEmail(email), "Email Invalido"),
+            (validadores.validarTelefoneFixo(telefone), "Telefone Fixo Invalido"),
+            (validadores.validarData(dataNascimento), "Data de nascimento Invalida"),
+            (validadores.validarCelular(celular), "Celular Invalido"),
             ( Cliente.query.filter_by(CPF_CNPJ=CPF_CNPJ).first() is None,"Ja existe um usario com esse CPF/CNPJ" )
         ]
     )
@@ -311,11 +311,11 @@ def atualizarCadastroCliente(id):
     resultado,mensagem = pipelineValidacoes.Executar(
         [
             
-            (validarCPF_CNPJ(CPF_CNPJ), "CPF ou CNPJ invalido."),
-            (validarEmail(email), "Email Invalido"),
-            (validarTelefoneFixo(telefone), "Telefone Fixo Invalido"),
-            (validarData(dataNascimento), "Data de nascimento Invalida"),
-            (validarCelular(celular), "Celular Invalido"),
+            (validadores.validarCPF_CNPJ(CPF_CNPJ), "CPF ou CNPJ invalido."),
+            (validadores.validarEmail(email), "Email Invalido"),
+            (validadores.validarTelefoneFixo(telefone), "Telefone Fixo Invalido"),
+            (validadores.validarData(dataNascimento), "Data de nascimento Invalida"),
+            (validadores.validarCelular(celular), "Celular Invalido"),
         ]
     )
     if resultado is False: 
@@ -338,205 +338,3 @@ def atualizarCadastroCliente(id):
     db.session.commit()
     
     return True
-
-
-def deletarCliente(id):
-    cliente_obj = Cliente.query.filter_by(id=id).first()
-    if cliente_obj is None:
-        codigo, mensagem = response.bad_request("Cliente Nao Encontrado")
-        abort(codigo, mensagem)
-    dataExclusao =  datetime.now()
-    setattr(cliente_obj, 'dataExclusao', dataExclusao)
-    db.session.commit()
-    return True
-    
-def validarCPF(cpf:str) -> bool:
-    if cpf is None:
-      return True
-    if cpf=='': 
-      return False
-    
-    cpf_standard = re.compile("[0-9]{3}[.][0-9]{3}[.][0-9]{3}[-][0-9]{2}$")
-    legal_format = cpf_standard.match(cpf)
-    if(not legal_format):
-      return False
-    
-    cpf = cpf.replace('.', '')
-    cpf = cpf.replace('-', '')
-    init_cpf = cpf
-    cpf = cpf[:9]
-    
-    checksum = 0
-    mult = 10
-    for num in range(9):
-        checksum += int(cpf[num]) * (mult - int(num))
-    rest = checksum % 11
-    if(rest<2):
-        first_digit = 0
-    else:
-        first_digit = 11-rest
-    cpf += str(first_digit)
-
-    checksum = 0
-    mult = 11
-    for num in range(10):
-        checksum += int(cpf[num]) * (mult - int(num))
-    rest = checksum % 11
-    if(rest<2):
-        second_digit = 0
-    else:
-        second_digit = 11-rest
-    cpf += str(second_digit)
-
-    return cpf==init_cpf
-
-def validarCNPJ(cnpj:str) -> bool:
-  if(cnpj is None):
-    return True
-  if cnpj == '': 
-      return False
-  
-  cnpj_standard = re.compile("[0-9]{2}[.][0-9]{3}[.][0-9]{3}[/][0-9]{4}[-][0-9]{2}$")
-  legal_format = cnpj_standard.match(cnpj)
-  if(not legal_format):
-    return False
-  
-  cnpj = cnpj.replace('.', '')
-  cnpj = cnpj.replace('-', '')
-  cnpj = cnpj.replace('/', '')
-  init_cnpj = cnpj
-  cnpj = cnpj[:12]
-
-  checksum = 0
-  mult = [5,4,3,2,9,8,7,6,5,4,3,2]
-  for num in range(12):
-    checksum += int(cnpj[num]) * mult[num]
-  rest = checksum % 11
-  if(rest<2):
-    first_digit = 0
-  else:
-    first_digit = 11 - rest
-  cnpj += str(first_digit)
-
-  checksum = 0
-  mult = [6,5,4,3,2,9,8,7,6,5,4,3,2]
-  for num in range(13):
-    checksum += int(cnpj[num]) * mult[num]
-  rest = checksum % 11
-  if(rest<2):
-    second_digit = 0
-  else:
-    second_digit = 11 - rest
-  cnpj += str(second_digit)
-
-  return cnpj==init_cnpj
-
-def validarCPF_CNPJ(cpfCnpj:str) -> bool:
-  return validarCPF(cpfCnpj) or validarCNPJ(cpfCnpj)
-
-def validarEmail(email:str) -> bool:
-  if(email==None):
-    return True
-  if email == '': 
-      return False
-  size = len(email)
-  at, dot, dot_before_at ,dot_after_at = 0, 0, 0, 0
-  for i in range(size):
-    symbol = email[i]
-    if(symbol=='@'):
-      if(at>0):
-        return False
-      at += 1
-      if(i<3):
-        return False
-    elif(at>0):
-      if(dot>0):
-        dot_after_at += 1
-      elif(symbol=='.'):
-        dot = 1
-        if(dot_before_at<3):
-          return False
-      else:
-        dot_before_at += 1
-  if(i+1==size and dot_after_at>1):
-    return True
-  else:
-    return False
-
-def validarCelular(celular:str) -> bool:
-  if(celular==None):
-    return True
-  if celular == '': 
-      return False
-  padrao = r'\(\d{2}\) (9\d{4}-\d{4}|\d{4}-\d{4})'  # padrão regex para '(XX) 9XXXX-XXXX' ou '(XX) XXXX-XXXX'
-
-  if re.match(padrao, celular):
-      return True
-  else:
-      return False
-
-# Validadores
-
-def validarTelefoneFixo(telefoneFixo: str) -> bool:
-  if(telefoneFixo==None):
-    return True
-  if telefoneFixo == '': 
-      return False
-  telefoneFixo = telefoneFixo.replace('+', '')
-  telefoneFixo = telefoneFixo.replace('(', '')
-  telefoneFixo = telefoneFixo.replace(')', '')
-  telefoneFixo = telefoneFixo.replace('-', '')
-  telefoneFixo = telefoneFixo.replace(' ', '')
-  telefoneFixo = telefoneFixo.removeprefix('0')
-
-  size = len(telefoneFixo)
-  
-  ddds = re.compile("(1[1-9]|2[12478]|3[1-578])|4[1-9]|5[13-5]|6[1-9]|7[13-579]|8[1-9]|9[1-9]")
-  valid_ddd = ddds.match(telefoneFixo)
-  if(not valid_ddd):
-    return False
-  
-  if(size!=10):
-    return False
-
-  valid_prefixes = ['2','3','4','5']
-  if(telefoneFixo[-8] not in valid_prefixes):
-    return False
-  
-  return True
-
-def validarCEP(cep: str) -> bool:
-  if(cep==None):
-    return True
-  
-  cep_standard = re.compile("[0-9]{2}[.][0-9]{3}[-][0-9]{3}$")
-  legal_format = cep_standard.match(cep)
-  if(not legal_format):
-    return False
-  return True
-
-def validarCodigoBarras(codigoBarras: str) -> bool:
-  return True 
-
-def validarData(data: str) -> bool:
-  if data == None: 
-      return True
-  if data == '': 
-      return False
-  formato = "%Y-%m-%d"
-  try:
-      datetime.strptime(data, formato)
-      return True
-  except ValueError:
-      return False
-  
-def validarEstado(estado: str) -> bool:
-  if estado is None:
-    return False
-  estados = {'AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO'}
-  return estado.upper() in estados
-
-def validarNumero(numero: str) -> bool:
-  if numero is None:
-    return False
-  return numero.isnumeric()
---
+++ packages/validadores.py
@@ -0,0 +1,194 @@
+import re
+from datetime import datetime
+
+
+def validarCPF(cpf:str) -> bool:
+    if cpf is None:
+      return True
+    if cpf=='': 
+      return False
+    
+    cpf_standard = re.compile("[0-9]{3}[.][0-9]{3}[.][0-9]{3}[-][0-9]{2}$")
+    legal_format = cpf_standard.match(cpf)
+    if(not legal_format):
+      return False
+    
+    cpf = cpf.replace('.', '')
+    cpf = cpf.replace('-', '')
+    init_cpf = cpf
+    cpf = cpf[:9]
+    
+    checksum = 0
+    mult = 10
+    for num in range(9):
+        checksum += int(cpf[num]) * (mult - int(num))
+    rest = checksum % 11
+    if(rest<2):
+        first_digit = 0
+    else:
+        first_digit = 11-rest
+    cpf += str(first_digit)
+
+    checksum = 0
+    mult = 11
+    for num in range(10):
+        checksum += int(cpf[num]) * (mult - int(num))
+    rest = checksum % 11
+    if(rest<2):
+        second_digit = 0
+    else:
+        second_digit = 11-rest
+    cpf += str(second_digit)
+
+    return cpf==init_cpf
+
+def validarCNPJ(cnpj:str) -> bool:
+  if(cnpj is None):
+    return True
+  if cnpj == '': 
+      return False
+  
+  cnpj_standard = re.compile("[0-9]{2}[.][0-9]{3}[.][0-9]{3}[/][0-9]{4}[-][0-9]{2}$")
+  legal_format = cnpj_standard.match(cnpj)
+  if(not legal_format):
+    return False
+  
+  cnpj = cnpj.replace('.', '')
+  cnpj = cnpj.replace('-', '')
+  cnpj = cnpj.replace('/', '')
+  init_cnpj = cnpj
+  cnpj = cnpj[:12]
+
+  checksum = 0
+  mult = [5,4,3,2,9,8,7,6,5,4,3,2]
+  for num in range(12):
+    checksum += int(cnpj[num]) * mult[num]
+  rest = checksum % 11
+  if(rest<2):
+    first_digit = 0
+  else:
+    first_digit = 11 - rest
+  cnpj += str(first_digit)
+
+  checksum = 0
+  mult = [6,5,4,3,2,9,8,7,6,5,4,3,2]
+  for num in range(13):
+    checksum += int(cnpj[num]) * mult[num]
+  rest = checksum % 11
+  if(rest<2):
+    second_digit = 0
+  else:
+    second_digit = 11 - rest
+  cnpj += str(second_digit)
+
+  return cnpj==init_cnpj
+
+def validarCPF_CNPJ(cpfCnpj:str) -> bool:
+  return validarCPF(cpfCnpj) or validarCNPJ(cpfCnpj)
+
+def validarEmail(email:str) -> bool:
+  if(email==None):
+    return True
+  if email == '': 
+      return False
+  size = len(email)
+  at, dot, dot_before_at ,dot_after_at = 0, 0, 0, 0
+  for i in range(size):
+    symbol = email[i]
+    if(symbol=='@'):
+      if(at>0):
+        return False
+      at += 1
+      if(i<3):
+        return False
+    elif(at>0):
+      if(dot>0):
+        dot_after_at += 1
+      elif(symbol=='.'):
+        dot = 1
+        if(dot_before_at<3):
+          return False
+      else:
+        dot_before_at += 1
+  if(i+1==size and dot_after_at>1):
+    return True
+  else:
+    return False
+
+def validarCelular(celular:str) -> bool:
+  if(celular==None):
+    return True
+  if celular == '': 
+      return False
+  padrao = r'\(\d{2}\) (9\d{4}-\d{4}|\d{4}-\d{4})'  # padrão regex para '(XX) 9XXXX-XXXX' ou '(XX) XXXX-XXXX'
+
+  if re.match(padrao, celular):
+      return True
+  else:
+      return False
+
+
+
+def validarTelefoneFixo(telefoneFixo: str) -> bool:
+  if(telefoneFixo==None):
+    return True
+  if telefoneFixo == '': 
+      return False
+  telefoneFixo = telefoneFixo.replace('+', '')
+  telefoneFixo = telefoneFixo.replace('(', '')
+  telefoneFixo = telefoneFixo.replace(')', '')
+  telefoneFixo = telefoneFixo.replace('-', '')
+  telefoneFixo = telefoneFixo.replace(' ', '')
+  telefoneFixo = telefoneFixo.removeprefix('0')
+
+  size = len(telefoneFixo)
+  
+  ddds = re.compile("(1[1-9]|2[12478]|3[1-578])|4[1-9]|5[13-5]|6[1-9]|7[13-579]|8[1-9]|9[1-9]")
+  valid_ddd = ddds.match(telefoneFixo)
+  if(not valid_ddd):
+    return False
+  
+  if(size!=10):
+    return False
+
+  valid_prefixes = ['2','3','4','5']
+  if(telefoneFixo[-8] not in valid_prefixes):
+    return False
+  
+  return True
+
+def validarCEP(cep: str) -> bool:
+  if(cep==None):
+    return True
+  
+  cep_standard = re.compile("[0-9]{2}[.][0-9]{3}[-][0-9]{3}$")
+  legal_format = cep_standard.match(cep)
+  if(not legal_format):
+    return False
+  return True
+
+def validarCodigoBarras(codigoBarras: str) -> bool:
+  return True 
+
+def validarData(data: str) -> bool:
+  if data == None: 
+      return True
+  if data == '': 
+      return False
+  formato = "%Y-%m-%d"
+  try:
+      datetime.strptime(data, formato)
+      return True
+  except ValueError:
+      return False
+  
+def validarEstado(estado: str) -> bool:
+  if estado is None:
+    return False
+  estados = {'AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO'}
+  return estado.upper() in estados
+
+def validarNumero(numero: str) -> bool:
+  if numero is None:
+    return False
+  return numero.isnumeric()
