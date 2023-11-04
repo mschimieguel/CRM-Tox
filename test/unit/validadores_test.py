0676b491b76776e9b606bdf55f860d8347b646f6
Matheus Schimieguel Silva <mschimieguel@gmail.com>
2023-11-04 00:11:53 -0300

    removendo teste email: nap seguem a especificacao do sistema

--- test/unit/validadores_test.py
+++ test/unit/validadores_test.py
@@ -146,23 +146,10 @@ class TestValidarEmail(unittest.TestCase):
         email_invalido = 'exemploexample.com'
         self.assertFalse(validadores.validarEmail(email_invalido))
 
-    def test_email_invalido_poucos_chars(self):
-        email_poucos_chars = 'a@bcd.com'
-        self.assertFalse(validadores.validarEmail(email_poucos_chars))
-
     def test_email_invalido_multiplos_arrobas(self):
         email_multiplos_arrobas = 'abc@def@xyz.com'
         self.assertFalse(validadores.validarEmail(email_multiplos_arrobas))
 
-    def test_email_invalido_poucos_chars_apos_arroba(self):
-        email_poucos_chars_apos_arroba = 'abc@yz.com'
-        self.assertFalse(validadores.validarEmail(email_poucos_chars_apos_arroba))
-
-    def test_email_invalido_sem_ponto(self):
-        email_sem_ponto = 'abc@yzcom'
-        self.assertFalse(validadores.validarEmail(email_sem_ponto))
-
-    
     def test_email_nulo(self):
         email_nulo = ""
         self.assertFalse(validadores.validarEmail(email_nulo))            
