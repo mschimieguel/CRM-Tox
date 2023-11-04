d071c4b6adfe4e3d956ba6e5abe7345ee34d645b
Matheus Schimieguel Silva <mschimieguel@gmail.com>
2023-11-04 17:49:29 -0300

    Refactoring: removendo comentarios e comented code

--- app/__init__.py
+++ app/__init__.py
@@ -8,11 +8,10 @@ from app.models.rdb import db
 from werkzeug.middleware.proxy_fix import ProxyFix
 from flask_restx import Api,Namespace,Resource
 
-# Globally accessible libraries
 ma = Marshmallow()
-# Application Factory App
+
 def create_app(test_config=None) -> Flask:
-    """Create an app by initializing components"""
+
     app = Flask(__name__)
     app.wsgi_app = ProxyFix(app.wsgi_app)
     if( test_config):
@@ -27,46 +26,23 @@ def create_app(test_config=None) -> Flask:
     @app.route('/swagger/<path:path>')
     def send_swagger_static(path):
         return send_from_directory('swagger', path)
-    
-    # Initialize database and marshmallow
+
     db.init_app(app)
     ma.init_app(app)
     
-    # Enable CORS for all routes
     CORS(app)
     
     with app.app_context():
         db.create_all()  
 
-
-
-        # Include our Routes in our context
         from app.routes import cliente, Produto, enderecoscliente
         from app.routes import nsCliente, nsEnderecoCliente,nsProduto
-
-       
-        # Register Blueprints
-        #app.register_blueprint(cliente)
-        #app.register_blueprint(Produto)
-        # app.register_blueprint(enderecoscliente)
-        
-        
+      
         api = Api(app, version='1.0', title='Simple CRM', description='API documentation',doc="/swagger")
         api.add_namespace(nsCliente)
         api.add_namespace(nsEnderecoCliente)
         api.add_namespace(nsProduto)
         
-       
         return app
-# def createApi(app):
-#     return Api(app, version='1.0', title='Simple CRM', description='API documentation',doc="/swagger")
 
 app = create_app()
-
-
-
-# @api.route('/hello')
-# class HelloResource(Resource):
-#     def get(self):
-#             """An example endpoint that returns a greeting."""
-#             return "Hello, World!Te"
